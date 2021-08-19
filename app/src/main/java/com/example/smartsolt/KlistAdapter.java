9d7e094f13aa6ea5bd724e5b21f3a2a61ce44147
한창희 <ckdgml0603@naver.com>
2021-08-19 16:20:21 +0900

    klist추가

--- app/src/main/java/com/example/smartsolt/KlistAdapter.java
+++ app/src/main/java/com/example/smartsolt/KlistAdapter.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class ZlistAdapter extends BaseAdapter {
+public class KlistAdapter extends BaseAdapter {
 
     Context context;
     int layout;
@@ -18,7 +18,7 @@ public class ZlistAdapter extends BaseAdapter {
 
     LayoutInflater inflater;
 
-    public  ZlistAdapter(Context context, int layout, List<ZlistVO> data){
+    public KlistAdapter(Context context, int layout, List<ZlistVO> data){
         this.context = context;
         this.layout = layout;
         this.data = data;
@@ -44,16 +44,16 @@ public class ZlistAdapter extends BaseAdapter {
             convertView = inflater.inflate(layout, null);
         }
 
-        TextView z_numbering = convertView.findViewById(R.id.tv_z_numbering);
-        TextView z_indoorTemp = convertView.findViewById(R.id.tv_z_indoor_temp);
-        TextView z_waterTemp = convertView.findViewById(R.id.tv_z_water_temp);
-        Button btn_move_sen = convertView.findViewById(R.id.btn_move_sen);
+        TextView z_numbering = convertView.findViewById(R.id.tv_k_numbering);
+        TextView z_indoorTemp = convertView.findViewById(R.id.tv_k_indoor_temp);
+        TextView z_waterTemp = convertView.findViewById(R.id.tv_k_water_temp);
+        Button btn_move_sen_1 = convertView.findViewById(R.id.btn_move_sen_1);
 
         z_numbering.setText(data.get(position).getZ_numbering());
         z_indoorTemp.setText(data.get(position).getZ_indoor_temp_now());
         z_waterTemp.setText(data.get(position).getZ_water_temp_now());
 
-        btn_move_sen.setOnClickListener(new View.OnClickListener() {
+        btn_move_sen_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Fragment로 넘어가기 위해서는 MainActivity에 만들어둔 changeFrament에 접근
